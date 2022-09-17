263b6f82bda16b60c091e2fcf64133964d7d69ed
hugo.damasceno <esmasiadamente@gmail.com>
2022-09-17 17:46:56 -0300

    Lista de professores com ngFor e ngIf

--- src/app/pages/table-teacher/table-teacher.component.ts
+++ src/app/pages/table-teacher/table-teacher.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+
+import { ITeacher } from 'src/app/models/teacher';
 
 @Component({
   selector: 'app-table-teacher',
@@ -11,5 +14,85 @@ export class TableTeacherComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
+  teachers: ITeacher[] = [
+    {
+      name: "Danilo Aparecido",
+      phoneNumber: "81998969698",
+      email: "torneseumprogramador@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "São Bernardo do Campo",
+      street: "Rua Augusta a 120 km/h",
+      postalCode: "00000-00",
+      state: "SP"
+    },
+    {
+      name: "Claudio Rapôso",
+      phoneNumber: "81998969698",
+      email: "claudio.raposo@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "Recife",
+      street: "Rua Urbana",
+      postalCode: "00000-00",
+      state: "PE"
+    },
+    {
+      name: "Victor Farias",
+      phoneNumber: "81998969698",
+      email: "victor.farias@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "São Paulo",
+      street: "Rua Placeholder",
+      postalCode: "00000-00",
+      state: "SP"
+    },
+    {
+      name: "Isidro Massetto",
+      phoneNumber: "81998969698",
+      email: "professorisidro@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "São Paulo",
+      street: "150th Avenue NE",
+      postalCode: "00000-00",
+      state: "SP"
+    },
+    {
+      name: "Lylian Toledo",
+      phoneNumber: "81998969698",
+      email: "lylian.toledo@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "São Paulo",
+      street: "Rua Genérica",
+      postalCode: "00000-00",
+      state: "SP"
+    },
+    {
+      name: "Guilherme Junqueira",
+      phoneNumber: "81998969698",
+      email: "guilherme.junqueira@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "São Paulo",
+      street: "Rua Leadership",
+      postalCode: "00000-00",
+      state: "SP"
+    },
+    {
+      name: "Hiara Peixoto",
+      phoneNumber: "81998969698",
+      email: "hiara.peixoto@dominio.com.br",
+      salary: "R$ 10.000.000,00",
+      country: "Brasil",
+      city: "São Paulo",
+      street: "Rua Gestão de Pessoas",
+      postalCode: "00000-00",
+      state: "SP"
+    }
+  ];
+  
 }
